33ebf90ac776f191117b818faeb51e77dc0a249d
orvi2014 <das.orvi@yahoo.com>
2018-09-15 00:46:05 +0600

    First version complete

---
+++ spec/javafetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JavafetchView from '../lib/javafetch-view';
+
+describe('JavafetchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
